9f985fc2d770cd3765950b810ed9406cbf6ef8c1
SusanBird <susanbird391@gmail.com>
2022-05-18 16:55:32 -0700

    basic emoji test working

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Open/i);
+  const linkElement = screen.getByText(/animals/i);
   expect(linkElement).toBeInTheDocument();
 });
