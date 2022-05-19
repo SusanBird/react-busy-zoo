526beff53873b576de38221f59dd7603572da1e7
SusanBird <susanbird391@gmail.com>
2022-05-18 18:44:21 -0700

    added charts and table

--- src/MyCharts.js
+++ src/MyCharts.js
@@ -1,7 +1,40 @@
+
+   
 import React from 'react';
+import { VictoryBar, VictoryChart, VictoryPie } from 'victory';
 
 export default function MyCharts() {
   return (
-    <div>MyCharts</div>
+    <div>
+      <VictoryChart
+        domainPadding={40}
+      >
+        <VictoryBar
+          width={500}
+          data={[
+            { quarter: 1, earnings: 13000 },
+            { quarter: 2, earnings: 16500 },
+            { quarter: 3, earnings: 14250 },
+            { quarter: 4, earnings: 19000 }
+          ]}
+          x="quarter"
+          y="earnings"
+        />
+      </VictoryChart>
+  
+      <VictoryPie
+        width={500}
+        colorScale={['green', 'violet', 'lightblue', 'coral']}
+        data={[
+          { age: '1-3', students: 43 },
+          { age: '4-7', students: 132 },
+          { age: '8-12', students: 322 },
+          { age: '13-18', students: 573 }
+        ]}
+        x="age"
+        y="students"
+      />
+
+    </div>
   );
 }
--- src/MyTable.js
+++ src/MyTable.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import MaterialTable from 'material-table';
+import { customers } from './data.js';
 
 export default function MyTable() {
   return (
-    <div>MyTable</div>
+    <div style={{ maxWidth: '75%' }}>
+      <MaterialTable
+        columns={
+          [
+            { title: 'Id', field: 'id' },
+            { title: 'First Name', field: 'first_name' },
+            { title: 'Last Name', field: 'last_name' },
+            { title: 'Email', field: 'email' },
+            { title: 'Gender', field: 'gender' },
+            { title: 'Computer Address Host', field: 'ip_address' }
+          ]
+        }
+        data={customers}
+    
+        title="Customer Data"
+        options={{  
+          headerStyle: { 
+            background: 'navy',
+          },
+          rowStyle: { 
+            background: 'orange',
+          }
+        }
+        }
+        localization={{
+          pagination: {
+            nextAriaLabel: 'next',
+            previousAriaLabel: 'prev'
+          } 
+        }}
+
+      />      </div>
   );
 }
